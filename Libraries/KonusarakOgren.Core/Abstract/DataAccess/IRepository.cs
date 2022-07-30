fd0e32c29b4df8405c28b36d0db1ca3eb7085b70
Muhammed Ali Ragib CAKIR <muhammed.cakir@bilgeadam.com>
2022-07-30 17:19:23 +0300

    init

---
+++ Libraries/KonusarakOgren.Core/Abstract/DataAccess/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using KonusarakOgren.Core.Abstract.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonusarakOgren.Core.Abstract.DataAccess
+{
+    public interface IRepository<T> where T : class, IBaseEntity
+    {
+        Task<bool> Add(T entity);
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<T> FirstOrDefault(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAll();
+        Task<List<T>> GetAll(Expression<Func<T, bool>> expression);
+        Task<T> Get(Expression<Func<T, bool>> expression);
+        Task<T> GetByID(Guid id);
+        Task<int> Save();
+    }
+}
---
+++ Libraries/KonusarakOgren.Core/Enums/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonusarakOgren.Core.Enums
+{
+    public enum Status
+    {
+        [Display(Name = "Active")]
+        Active = 1,
+        [Display(Name = "Modified")]
+        Modified,
+        [Display(Name = "Passive")]
+        Passive,
+    }
+}
